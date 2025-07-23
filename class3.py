823ad03ef295daf5f5470bdf532816b094ccd96f
Fourcuz <focus.senphab@gmail.com>
2025-07-23 11:17:02 +0700

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,46 @@
+# # score = int(input("can u put score : "))
+# # if score >=0:
+# #     if score <= 100:
+# #         if score >=80:
+# #             print("A")
+# #         if score < 80:
+# #             if score >= 70:
+# #                 print("B")
+# #             if score < 70:
+# #                 if score >= 60:
+# #                     print("C")
+# #                 if score < 60:
+# #                     if score >= 50:
+# #                         print("D")
+# #                     if score < 50:
+# #   
+# # 
+# #                       print("F")
+# lit = []
+# i = True    
+# while i == True:
+#    lit.append(input("name : "))
+#    if lit[-1] == "*":
+#       lit.remove(lit[-1])
+#       break
+# print(lit)  
+                   
+   
+
+# # while True:
+# #    choice = int(input("Enter 1 for + Enter 2 for exit"))
+
+# #    if choice == 1:
+# #       num = int(input("เลขที่ต้องการจะบวก"))
+# #       sumation = 0
+
+# #       for i in range(num):
+# #          num1 = int(input("กรอกเลข :"))
+# #          sumation += num1
+      
+# #       print("ผลลัพ : "sumation)
+    
+# #    if choice == 2:
+# #       break
+    
+    
